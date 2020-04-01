6b578cc27252cfe0b26980e1638efee29e04aec8
Samuel Otterman <sjotterman@gmail.com>
2020-04-01 11:38:19 -0400

    allow origin url

--- pages/api/name.js
+++ pages/api/name.js
@@ -2,5 +2,6 @@ import { randomName } from "../../src/generator";
 
 export default (req, res) => {
   const name = randomName();
+  res.setHeader('Access-Control-Allow-origin', 'https://namr.samuelotterman.com');
   res.status(200).json({ name });
 };
